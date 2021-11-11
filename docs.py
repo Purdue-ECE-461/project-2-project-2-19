dd3fd83665d993ebefe1dee17c297e923dadb5eb
ajay jairath <gjairath@purdue.edu>
2021-11-11 02:44:16 -0500

    Added docs to keep my progress in track

---
+++ docs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 02:29:57 2021
+
+@author: garvi
+"""
+
+'''
+Baseline requirement
+    You must support, via a REST-ful API:
+    -	Upload, update, rate, and download individual packages, including the new metric
+    -	Ingestion of a public npm package as described
+    -	Paginated list of all packages
+    -	Reset to default system state
+
+Progress:
+    [Note to self: Dont change file location]
+        file:///C:/Users/garvi/Downloads/P2M1n%20(3).pdf
+
+Todos:
+    1. Set up CI/CD.
+    2. SQL + Cloud deployment to support package upload WITHOUT metric calculations.
+    3. Revise the front end to support animations, a navigation bar, and fade ins.
+    4. Do the metric calculations on a BASE-64 encoded zip file. 
+
+    5. Any other BASE-line requirement.
+'''
