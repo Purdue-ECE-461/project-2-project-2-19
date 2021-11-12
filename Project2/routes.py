dbb44c2eb06b4ade7f439801dc2fc07a35d508bf
ajay jairath <gjairath@purdue.edu>
2021-11-12 04:57:29 -0500

    Updated the UI, working on uploading packages

--- Project2/routes.py
+++ Project2/routes.py
@@ -6,14 +6,25 @@ Created on Thu Nov 11 01:22:59 2021
 """
 
 from Project2 import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
+@app.route("/home")
 def homepage():
-    return render_template("index.html", title="HOME PAGE")
+    return render_template("index.html", title="NPM-Registry Group 19")
 
 @app.route("/docs")
 def docs():
+    return render_template("index.html", title="docs page")
+
+@app.route("/upload", methods=['POST', 'GET'])
+def upload():
+    print (request)
+    if request.method == 'POST':
+        print ("Testing")
+        f = request.files['file']
+        print (f)
+        
     return render_template("index.html", title="docs page")
 
 @app.route("/about")
