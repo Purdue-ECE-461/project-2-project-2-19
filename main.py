d72fa5abf3c7d08e79b6936e99134b0f1d8bc19e
Saumyanavani <saumyanavani2000@gmail.com>
2021-11-14 15:50:07 -0500

    Fixed main to deploy to App Engine.
    Setting the secret key inside the if block for main was causing issues and was not setting the secret key or config. Moved every statement except run outside the if block to fix it. Successful deployment at 3:50 pm, 14 Nov. 2021.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Nov 11 01:24:05 2021
 
-@author: garvi
+@author: garvi, navani
 """
 
 
@@ -13,8 +13,8 @@ import os
 load_dotenv()
 
 app.add_api('swagger.yaml')
+flask_app = app.app
+flask_app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
-    flask_app = app.app
-    flask_app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.run(host='0.0.0.0', port=5000, debug=True)
