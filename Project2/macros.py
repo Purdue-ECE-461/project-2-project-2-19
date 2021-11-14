edbedc34562070b556722e8080f465c1bd7fffc0
ajay jairath <gjairath@purdue.edu>
2021-11-13 23:49:38 -0500

    Front end upload works, beginning API

---
+++ Project2/macros.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 23:04:33 2021
+
+@author: garvi
+"""
+
+'''
+    This file has all the config variables at the top level.
+'''
+
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
--- Project2/routes.py
+++ Project2/routes.py
@@ -5,8 +5,16 @@ Created on Thu Nov 11 01:22:59 2021
 @author: garvi
 """
 
+#File imports
 from Project2 import app
+from Project2 import util
+from Project2 import macros
+
+#Library imports
+import os
 from flask import render_template, request
+from google.cloud import storage
+
 
 @app.route("/")
 @app.route("/home")
@@ -19,11 +27,39 @@ def docs():
 
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
-    print (request)
+    '''
+    Code borrowed from google-docs.
+        https://cloud.google.com/appengine/docs/flexible/python/using-cloud-storage
+    '''
     if request.method == 'POST':
-        print ("Testing")
+        
+        #Verify the auth works.
+        util.implicit()
+        
         f = request.files['file']
-        print (f)
+        
+        if not f:
+            return 'No file uploaded.', 400
+
+        gcs = storage.Client()
+        # Get the bucket that the file will be uploaded to.
+        bucket = gcs.get_bucket(macros.CLOUD_STORAGE_BUCKET)
+    
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(f.filename)
+    
+        blob.upload_from_string(
+            f.read(),
+            content_type=f.content_type
+        )
+    
+        # Make the blob public. This is not necessary if the
+        # entire bucket is public.
+        # See https://cloud.google.com/storage/docs/access-control/making-data-public.
+        blob.make_public()
+    
+        # The public URL can be used to directly access the uploaded file via HTTP.
+        print(blob.public_url)
         
     return render_template("index.html", title="docs page")
 
