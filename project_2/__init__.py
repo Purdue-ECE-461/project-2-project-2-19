5387920e063a11286c0b22cc31ab1b9de9baa5bc
Saumyanavani <saumyanavani2000@gmail.com>
2021-11-14 19:04:40 -0500

    Made changes to test with and pass CI pylint inspection

--- project_2/__init__.py
+++ project_2/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import connexion
-from project_2 import routes
+#from project_2 import routes
 
 # app = Flask(__name__)
 app = connexion.App(__name__, specification_dir='./')
